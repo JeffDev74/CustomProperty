cec7256c6c4aff341a82add0f3043b9a343d55c1
JeffDev <jeffdev74@gmail.com>
2017-03-03 23:17:40 -0800

    updated tests to reflect code chagnes
    see preview commit

--- Assets/ItemSystem/Tests/ItemTests.cs
+++ Assets/ItemSystem/Tests/ItemTests.cs
@@ -11,13 +11,17 @@ namespace FPS
 
         private void Start()
         {
+            // Test create new custom string
             StringProperty MyCustomString = new StringProperty("campfire_ss", "I am a campfire.");
-            (campfire.Data as WorldData).Properties.AddProp(MyCustomString);
+            (campfire.Data as WorldData).Properties.Add(MyCustomString);
 
-            string description = (campfire.Data as WorldData).Properties.GetProp<string>("campfire_ss");
-
+            // Test get custom string value
+            string description = (campfire.Data as WorldData).Properties.Get<string>("campfire_ss");
             Debug.Log("The saved value is [" + description + "]");
 
+            // Test get non-existing key
+            description = (campfire.Data as WorldData).Properties.Get<string>("none");
+            Debug.Log("The saved value is [" + description + "]");
 
         }
     }
